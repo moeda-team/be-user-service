066f95adf2251dc8119bc6baebcdc14b82f3190a
lovanto <rifkylovanto@gmail.com>
2025-05-08 00:21:08 +0700

    implement the jwt into endpoint

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import { validateCreateUser, validateUpdateUser } from '../middleware';
 import { HealthController } from '../controllers/health.controller';
 import { basicAuth } from '../middleware';
 import { AuthController } from '../controllers/auth.controller';
+import { jwtAuth } from '../middleware/jwtAuth.middleware';
 
 const router = Router();
 const userController = new UserController();
@@ -11,11 +12,11 @@ const healthController = new HealthController();
 const authController = new AuthController();
 
 router.get('/health', basicAuth, healthController.check);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', validateCreateUser, userController.createUser);
-router.put('/:id', validateUpdateUser, userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/', jwtAuth, userController.getAllUsers);
+router.get('/:id', jwtAuth, userController.getUserById);
+router.post('/', jwtAuth, validateCreateUser, userController.createUser);
+router.put('/:id', jwtAuth, validateUpdateUser, userController.updateUser);
+router.delete('/:id', jwtAuth, userController.deleteUser);
 router.post('/login', authController.login);
 
 export default router;
