b84c30fd786c44a2622d10039d60c91894e7fd0b
lovanto <rifkylovanto@gmail.com>
2025-05-10 19:09:47 +0700

    change the axios to more flexible

--- src/utils/axios.custom.ts
+++ src/utils/axios.custom.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
 
-export const axiosGet = async (req: Request, res: Response, params: string) => {
+export const axiosGet = async (req: Request, res: Response, url: string) => {
   try {
-    const response = await axios.get(`${process.env.USER_SERVICE_URL}/users/${params}`, {
+    const response = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${req.headers.authorization?.split(' ')[1]}`,
       },
     });
-    return response;
+    return response.data;
   } catch (error) {
     const err = error as Error & { response: { status: number; data: string } };
     return { status: err.response.status, data: err.response.data };
