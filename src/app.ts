d3e14864a66542ceb28ab155cc79a9478716978f
lovanto <rifkylovanto@gmail.com>
2025-05-24 14:32:10 +0700

    error handle to make it stay alive

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,12 @@ import { config } from './config';
 import { errorHandler, notFoundHandler } from './middleware';
 import { routes } from './routes';
 import { logger } from './utils/logger';
+import timeout from 'connect-timeout';
+import { ResponseHandler } from './utils/response/responseHandler';
 
 const app = express();
+
+app.use(timeout('5s'));
 
 app.use(helmet());
 app.use(
@@ -18,6 +22,15 @@ app.use(
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, _) => {
+  if (!req.timedout) {
+    return ResponseHandler.error(res, {
+      message: 'Internal server error',
+      statusCode: 500,
+    });
+  }
+});
 
 app.use(config.apiPrefix, routes);
 app.use(notFoundHandler);
